3c750502784fbcea17109bef11cd0a8632a3adea
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-06-28 17:24:05 +0300

    change mysql statement

--- Clickky_Family/src/ua/kas/clickky_family/Controller.java
+++ Clickky_Family/src/ua/kas/clickky_family/Controller.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,42 +12,24 @@ import javafx.scene.control.Label;
 
 public class Controller {
 
-	@FXML Label label_name;
-	ArrayList<String> al_company_name = new ArrayList<String>();
-	ArrayList<String> al_phone = new ArrayList<String>();
-	ArrayList<String> al_table = new ArrayList<String>();
-	
-	public void press(ActionEvent e) throws SQLException{
+	@FXML
+	Label label_name;
+
+	public void press(ActionEvent e) throws SQLException {
 		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/clickky_family", "root", "root");
 		Statement rstat = conn.createStatement();
-		ResultSet result = rstat.executeQuery("SELECT DISTINCT company FROM first");
-		
-		while(result.next()){
-			al_company_name.add(result.getString("company"));
+		ResultSet result = rstat
+				.executeQuery("select first.company, COUNT(first.type)<10 and sum(first.type like '%table%')>=3 "
+						+ "and sum(first.type like '%phone%')>sum(first.type like '%table%') from first  group by first.company");
+		while (result.next()) {
+			if (result
+					.getString(
+							"COUNT(first.type)<10 and sum(first.type like '%table%')>=3 and sum(first.type like '%phone%')>sum(first.type like '%table%')")
+					.equals("1")) {
+				label_name.setText(result.getString("company"));
+			} else {
+				System.out.println(result.getString("company") + " - not correct!");
+			}
 		}
-		
-		java.sql.PreparedStatement myStmt;
-		
-		for(int i=0; i<al_company_name.size(); i++){
-			myStmt = conn.prepareStatement("SELECT type FROM first where company = ?");
-			myStmt.setString(1, al_company_name.get(i));
-			ResultSet result2 = myStmt.executeQuery();
-			while(result2.next()){
-				if(result2.getString("type").equals("phone")){
-					al_phone.add(result2.getString("type"));
-				}
-				if(result2.getString("type").equals("table")){
-					al_table.add(result2.getString("type"));
-				}
-			}
-			if((al_phone.size()>al_table.size()) && (al_table.size()>3) && ((al_table.size()+al_phone.size())<10)){
-				label_name.setText(al_company_name.get(i));
-			}
-			else{
-				System.out.println(al_company_name.get(i)+" - unsuitable!");
-			}
-			al_phone.clear();
-			al_table.clear();
-		}	
 	}
 }
